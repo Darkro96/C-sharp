cfbaadfdc26f99f918f8feb428e1cc5417277ec3
Darkro96 <nagy.mate960814@gmail.com>
2017-12-14 19:45:26 +0100

    Kynea numbers

---
+++ Kynea/Kynea/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kynea
+{
+    class Program
+    {
+        static bool Kynea(int a)
+        {
+
+            int i = 0;
+            while (i < a)
+            {
+                if (Math.Pow((Math.Pow(2, i) + 1), 2) - 2 == a)
+                    return true;
+                i++;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a number: ");
+            int a;
+            a = int.Parse(Console.ReadLine());
+            if (Kynea(a))
+                Console.WriteLine("This number is Kynea number.");
+            else
+                Console.WriteLine("This number isn't Kynea number.");
+            Console.ReadKey();
+        }
+    }
+}
